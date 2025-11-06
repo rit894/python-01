d9b79abcc2865fb3be3ddf842069db8c3e5f9663
rit894 <nagarithwikv@gmail.com>
2025-11-06 08:50:08 +0530

    updated logic 06-11-2025

---
+++ programing_problems_solution2.py
@@ -0,0 +1,20 @@
+'''Michael works as a marketing executive and spends his whole day travelling around 
+getting orders for his company. Sometimes the trips are short and often times he has to 
+travel large distances on his two wheelers. He decides his outfit based on the climate so 
+that he is comfortable when he is at work. The decision is made as follows: If the day 
+is cold and it is snowing, he wears a parka. If it is cold and not snowing, he wears a 
+jacket. If the day is not cold, He wears formal outfit. Design an automatic decision 
+making system to help Michael to choose his outfit, given a list of day type 
+combinations (cold and snowing).'''
+#conditionals and  logic operators 
+'''Simson works as a boiler controller in ‘Powerful Boiler Industries’ as a boiler operator 
+for power generation. There are certain guidelines for the operation of the boiler to work 
+in standard operating conditions. Any kind of abnormal function is notified by a 
+temperature indicator which measures the temperature in the boiler and sounds an 
+appropriate alarm.  
+a. If the temperature falls below 140 F for 5 minutes and the heating burner is off, 
+then the ‘under heat alarm’ signals.  
+b. If the temperature is between 140 F and 200 F and the heating burner is off, then 
+the ‘normal alarm’ signals.  
+c. If the temperature is greater than 200 F for more than 10 minutes and the heating 
+burner is on, ‘over heat alarm’ signals. '''
