d31e40c8ce3acb6574909ae644ee84837ee5b395
rit894 <nagarithwikv@gmail.com>
2025-11-13 22:04:58 +0530

    Add file handling examples and enhance existing content

---
+++ Theory practice/prac_File_Handling.py
@@ -0,0 +1,52 @@
+
+
+#  Alternative way to read a file
+'''with open('demofile.txt', 'r') as f:
+     print(f.read())'''
+#read file without using with statement
+#r"D:\2024\Diwali 2024\IMG.jpg"
+'''f=open('D:\\mydrive\\python\\Theory practice\\demofile.txt','r')
+print(f.read())
+f.close()'''
+# read only one line of the file 
+'''with open("demofile.txt") as f:
+  print(f.readline())'''
+#loop through the file line by line
+'''with open("demofile.txt") as f:
+  for x in f:
+    print(x)'''
+# Write Write to an existing file
+#append methode
+'''with open('demofile.txt','a') as f:
+    f.write('this file now has more content!')
+with open('demofile.txt') as f:
+    print(f.read())'''
+# wrtie methode
+'''with open("demofile.txt", "w") as f:
+  f.write("Woops! I have deleted the content!")
+with open("demofile.txt") as f:
+  print(f.read())'''
+#Create a New File
+'''"x" - Create - will create a file, returns an error if the file exists
+
+"a" - Append - will create a file if the specified file does not exists
+
+"w" - Write - will create a file if the specified file does not exists
+
+f = open("myfile.txt", "x")'''
+# Delete a File
+'''import os 
+os.remove("demofile.txt")
+
+import os
+
+file_path = r"D:\2024\Diwali 2024\IMG20241101063138.jpg"
+
+if os.path.exists(file_path):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        print("File read successfully. Size:", len(data), "bytes")
+else:
+    print("File not found. Please check the path or filename.")
+'''
+          
--- Theory practice/prac_Lambd_Functions.py
+++ Theory practice/prac_Lambd_Functions.py
@@ -1,6 +1,24 @@
-points2D=[(1,2),(5,6),(3,4),(7,-8)]
+'''points2D=[(1,2),(5,6),(3,4),(7,-8)]
 
 points2D_sorted=sorted(points2D, key=lambda x: x[1])
 
 print(points2D_sorted)
 print(points2D)
+'''
+#ambda function to convert a string to its upper case using upper()
+'''s1 = 'GeeksforGeeks'
+s2 = lambda func: func.upper()
+print(s2(s1))'''
+# lambda function uses nested if-else logic to classify numbers as Positive, Negative or Zero.
+'''n = lambda x: "Positive" if x > 0 else "Negative" if x < 0 else "Zero"
+print(n(5))   
+print(n(-3))  
+print(n(0))'''
+#Using with List Comprehension
+'''li=[lambda arg=x: arg*10 for x in range(1,6)]
+for item in li:
+    print(item())'''
+#Using for Returning Multiple Results
+'''x=lambda a,b: (a+b, a-b, a*b, a/b)
+result=x(10,5)
+print(result)'''
