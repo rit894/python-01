69eae8ec178c92398a0413806466f91671d9a8bd
rit894 <nagarithwikv@gmail.com>
2025-11-13 15:28:35 +0530

    Enhance factorial function with additional print statements for better traceability

--- Lab sessions/1_Factorial.py
+++ Lab sessions/1_Factorial.py
@@ -1,8 +1,11 @@
+
 def factorial(n):
+    print(f'the factorial function of {n} is called')
     if n==0:
        return 1
     else:
          x=n*factorial(n-1)
+         print(x,'is the factorial of ',n)
          return x
-print(factorial(5))  
+print(factorial(5))
       
