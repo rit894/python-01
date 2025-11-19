62711628b028b0be7254f57186a8d4adbc37b079
rit894 <nagarithwikv@gmail.com>
2025-11-19 22:33:36 +0530

    Update Updated_visited_links function to prevent duplicate new links and improve output format

---
+++ Examples/Eg_CPS_Data_structures_Q5.py
@@ -0,0 +1,7 @@
+def Updated_visited_links(visited_links,new_links):
+    
+    print(visited_links)
+x=input().split(' ')
+visited_links_set=set(x)
+new_links_list=input().split(' ')
+Updated_visited_links(visited_links_set,new_links_list)
