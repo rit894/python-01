6935b77468f5a24d70e37735f6ddb2bf5ad2870b
rit894 <nagarithwikv@gmail.com>
2025-11-26 23:00:38 +0530

    Add PSAT_Q40.py with implementation for counting 'z' and 'o' characters

---
+++ Examples/PSAT_Q/PSAT_Q40.py
@@ -0,0 +1,16 @@
+word=input().lower()
+z=0
+o=0
+for i in range(len(word)):
+    if word[i]=='z':
+        z+=1
+    elif word[i]=='o':
+        o+=1
+if 2*z==o:
+    print('yes')
+else:
+    print('no')
+# SET 2
+'''x=input()
+for i in range(9):
+    '''
