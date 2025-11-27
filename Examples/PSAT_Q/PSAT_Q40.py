812e96cea8dad5f015aeb36df43e072ef9f91c5e
rit894 <nagarithwikv@gmail.com>
2025-11-27 14:14:47 +0530

    Add PSAT_Q41.py for DDXDDD-DD format validation and create PSAT_Q42.py as a placeholder

--- Examples/PSAT_Q/PSAT_Q40.py
+++ Examples/PSAT_Q/PSAT_Q40.py
@@ -10,20 +10,4 @@ if 2*z==o:
     print('yes')
 else:
     print('no')
-# SET 2
-n=input('DDXDDD-DD')
-validity_list=[1,1,1,1,1]
-validity_check=[]
-if n[2] in "AEIOU":
-  validity_check.append(1)
-for i in range(len(n)):
-    if n[i].isdigit() and n[i+1].isdigit():
-        if n[i]==n[len(n)-1]:
-            continue
-        else:
-            (int(n[i])+int(n[i+1]))%2==0
-            validity_check.append(1)
-    else:
-        validity_check.append(0)
-print(validity_check)
 
