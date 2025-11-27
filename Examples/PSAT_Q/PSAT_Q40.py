e47ee0e8d5462f48f1c21784520a5a7a23321cf0
rit894 <nagarithwikv@gmail.com>
2025-11-27 08:53:03 +0530

    Implement input validation and checks for DDXDDD-DD format in PSAT_Q40.py

--- Examples/PSAT_Q/PSAT_Q40.py
+++ Examples/PSAT_Q/PSAT_Q40.py
@@ -11,6 +11,19 @@ if 2*z==o:
 else:
     print('no')
 # SET 2
-'''x=input()
-for i in range(9):
-    '''
+n=input('DDXDDD-DD')
+validity_list=[1,1,1,1,1]
+validity_check=[]
+if n[2] in "AEIOU":
+  validity_check.append(1)
+for i in range(len(n)):
+    if n[i].isdigit() and n[i+1].isdigit():
+        if n[i]==n[len(n)-1]:
+            continue
+        else:
+            (int(n[i])+int(n[i+1]))%2==0
+            validity_check.append(1)
+    else:
+        validity_check.append(0)
+print(validity_check)
+
